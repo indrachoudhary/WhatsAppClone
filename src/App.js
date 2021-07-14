9cca156ba08309c035ea9148c030ab43831e4ae2
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2021-07-14 17:31:19 +0530

    Login page for practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import styles, { Background, Title, Image, Input } from "./styles";
+import redblue from "./assets/redblue.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi React</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="Glass"> */}
+      <Title>LOGIN PAGE</Title>
+      <input placeholder="Enter your email" className="EmailInput" />
+      <input placeholder="Enter your password" className="PasswordInput" />
+      <button title="Login" className="LoginButton">
+        Login
+      </button>
+      <img src="https://wallpaperaccess.com/full/5113.jpg" className="Image" />
+      {/* </div> */}
     </div>
   );
 }
 
 export default App;
+
+/*
+<div className="App">
+      <Background>
+        <Title>Hello World</Title>
+        <img src={redblue} alt="this is an image" className="Image"></img>
+      </Background>
+    </div>
+*/
