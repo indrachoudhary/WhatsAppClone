a663139a966efe8cc5a239e763cae739c06cdf0a
indrachoudhary <73747827+indrachoudhary@users.noreply.github.com>
2021-07-16 19:02:27 +0530

    Created Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import {Avatar, IconButton, input } from '@material-ui/core'
 import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 import { SearchOutlined } from '@material-ui/icons'
+import SidebarChat from './SidebarChat'
 
 
 function Sidebar() {
@@ -31,6 +32,11 @@ function Sidebar() {
 <SearchOutlined/>
 <input placeholder="Search or start new chat" type="text"/>
 </div>
+</div>
+<div className="sidebar__chats">
+   <SidebarChat/> 
+   <SidebarChat/> 
+   <SidebarChat/> 
 </div>
         </div>
     )
